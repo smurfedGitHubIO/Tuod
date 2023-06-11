68fc724acf1d84f73c68dd7802023200358d8ff3
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2023-06-11 21:09:08 +0800

    feat: add other page svelte files

---
+++ src/routes/(restricted)/fake/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { page } from '$app/stores';
+    
+  const isBeta = $page.url.searchParams.get("svm");
+  const isAlpha = $page.url.searchParams.get("logistic_regression");
+</script>
+
+{#if isBeta}
+  <p>This is {isBeta}!</p>
+{:else}
+  <p>This is not beta.</p>
+{/if}
